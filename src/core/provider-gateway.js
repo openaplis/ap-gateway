e09517d66a042c19386f4c153239ee6712239429
William Copland <William.Copland@YPIILab.YPII.com>
2017-06-29 10:14:16 -0600

    Add provider gateway

---
+++ src/core/provider-gateway.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  getClientById: function(call, callback) {
+    var sql = ['Select * from tblClient ',
+      'where ClientId = \'' + call.request.clientId + '\';'].join('\n')
+    cmdSubmitter.submit(sql, function(err, client) [
+      if(err) return callback(err)
+      callback(null, { client: client } )
+    ])
+  }
+}
