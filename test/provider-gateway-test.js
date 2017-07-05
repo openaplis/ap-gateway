b72f4e2bbaeeb34d460a900b33ec24ac6112b356
SidHarder <softwarenavigator@gmail.com>
2017-07-05 09:25:57 -0600

    adding test

---
+++ test/provider-gateway-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const assert = require('chai').assert
+
+describe('Provider Gateway Test', function() {
+  it('Client Test', function(done) {
+    var searchDefinition = {
+      searchName: 'getByClientId',
+      searchParams: [{ name: 'clientId', value: '558' }]
+    }
+  })
+})
